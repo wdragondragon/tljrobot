cdcfa0187d9c5ee6af986e0737dff36f7995e1b4
zhengjinlong <1061917196@qq.com>
2022-11-12 13:31:50 +0800

    fix: 生僻字词库练习筛选词型有误

--- client/src/main/java/com/jdragon/tljrobot/client/utils/common/Code.java
+++ client/src/main/java/com/jdragon/tljrobot/client/utils/common/Code.java
@@ -1,71 +1,74 @@
 package com.jdragon.tljrobot.client.utils.common;
 
+import com.jdragon.tljrobot.tljutils.CodePointString;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Create by Jdragon on 2020.01.31
  */
 public class Code {
-    public HashMap<String,Integer> allTable = new HashMap<>();
-    public HashMap<String,Integer> otherTable = new HashMap<>();
-    public HashMap<String,Integer> firstTable = new HashMap<>();
+    public HashMap<CodePointString, Integer> allTable = new HashMap<>();
+    public HashMap<CodePointString, Integer> otherTable = new HashMap<>();
+    public HashMap<CodePointString, Integer> firstTable = new HashMap<>();
     private static Code code = null;
-    public static Code getInstance(String ciZuFileName){
-        if(code==null) {
+
+    public static Code getInstance(String ciZuFileName) {
+        if (code == null) {
             code = new Code(ciZuFileName);
         }
         return code;
     }
-    private Code(String ciZuFileName){
-        try{
+
+    private Code(String ciZuFileName) {
+        try {
             File more = new File(ciZuFileName);
             FileInputStream fis = new FileInputStream(more);
             InputStreamReader read = new InputStreamReader(fis, StandardCharsets.UTF_8);
             BufferedReader bufferRead = new BufferedReader(read);
             String str;
             String regex = "234567890";
-            while((str=bufferRead.readLine())!=null){
+            while ((str = bufferRead.readLine()) != null) {
                 String[] splited = str.split("\\s+");
-                if(splited.length!=2) {
+                if (splited.length != 2) {
                     continue;
                 }
-                String ch = splited[0];
+                CodePointString ch = new CodePointString(splited[0]);
                 String bm = splited[1];
                 String temp;
                 int length = splited[1].length();
-                temp = bm.substring(bm.length()-1);
-                if("_".equals(temp)||regex.contains(temp)) {
+                temp = bm.substring(bm.length() - 1);
+                if ("_".equals(temp) || regex.contains(temp)) {
                     length -= 1;
                 }
-                if(allTable.containsKey(splited[0])){
-                    if(allTable.get(splited[0])>length){
+                if (allTable.containsKey(ch)) {
+                    if (allTable.get(ch) > length) {
                         allTable.put(ch, length);
-                    }
-                    else if(allTable.get(splited[0])==length&&length==4){
-                        if(!regex.contains(temp)){
+                    } else if (allTable.get(ch) == length && length == 4) {
+                        if (!regex.contains(temp)) {
                             allTable.put(ch, length);
                         }
                     }
-                }
-                else{
+                } else {
                     allTable.put(ch, length);
                 }
-                if(regex.contains(temp)) {
+                if (regex.contains(temp)) {
                     if (otherTable.containsKey(ch)) {
-                        if (otherTable.get(splited[0]) > length) {
+                        if (otherTable.get(ch) > length) {
                             otherTable.put(ch, length);
                         }
                     } else {
                         otherTable.put(ch, length);
                     }
-                }else{
+                } else {
                     if (firstTable.containsKey(ch)) {
-                        if (firstTable.get(splited[0]) > length) {
+                        if (firstTable.get(ch) > length) {
                             firstTable.put(ch, length);
                         }
                     } else {
@@ -76,7 +79,7 @@ public class Code {
             bufferRead.close();
             read.close();
             fis.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("打开失败2");
             e.printStackTrace();
         }
